6e3c29311035fb97006f96f5ca01b8effe65ead3
WatCodeDatCode <arussell742@gmail.com>
2020-09-26 16:35:23 +0200

    Add form styling and save submission to localStorage

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,7 @@ const cityInput = document.getElementById("city-input");
 const countryInput = document.getElementById("country-input");
 const tripFromDateInput = document.getElementById("from-date-input");
 const tripToDateInput = document.getElementById("to-date-input");
-const summaryInput = document.getElementById("trip-summary-input");
+const tripSummaryInput = document.getElementById("trip-summary-input");
 
 // Variable for current date
 let today = new Date();
@@ -33,14 +33,62 @@ setMaxDateToCurrentDate();
 // Set min of "trip to date" to entered value of "trip from date" on change
 tripFromDateInput.onchange = function () {
   tripToDateInput.min = this.value;
+  tripToDateInput.value = this.value;
 };
 
 // *** FORM FUNCTIONS *** //
 const onSubmit = (event) => {
   event.preventDefault();
-  console.log(tripFromDateInput.value);
+
+  const city = cityInput.value;
+  const country = countryInput.value;
+  const from_date = tripFromDateInput.value;
+  const to_date = tripToDateInput.value;
+  const trip_summary = tripSummaryInput.value;
+
+//   localStorage.setItem("city", submittedCity);
+//   localStorage.setItem("country", submittedCountry);
+//   localStorage.setItem("from-date", submittedFromDate);
+//   localStorage.setItem("to-date", submittedToDate);
+//   localStorage.setItem("", submittedTripSummary);
+
+  const entry = {
+    city,
+    country,
+    from_date,
+    to_date,
+    trip_summary,
+  };
+
+  addEntry(entry);
   resetForm();
 };
+
+const addEntry = entry => {
+    saveEntry(entry);
+    // createEntriesHtml(); //TODO: Add function, also for singleEntriesHtml
+}
+
+const saveEntry = entry => {
+    const entries = getEntries();
+    entries.push(entry);
+  
+    const stringifiedEntries = JSON.stringify(entries);
+    localStorage.setItem("entries", stringifiedEntries);
+}
+
+const getEntries = () => {
+    const entries = localStorage.getItem("entries");
+
+    if (!entries) {
+      return [];
+    }
+  
+    const parsedEntries = JSON.parse(entries);
+    // const sortedEntries = sortEntries(parsedEntries);
+  
+    return parsedEntries;
+}
 
 const resetForm = () => {
   addEntryForm.reset();
