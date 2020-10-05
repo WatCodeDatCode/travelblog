822266726f78d8cc19f4aaed3c4f575c1921fba7
WatCodeDatCode <arussell742@gmail.com>
2020-10-05 23:57:53 +0200

    Fix nav responsiveness and modal background

--- scripts/script.js
+++ scripts/script.js
@@ -38,10 +38,10 @@ const navItemClass = document.querySelector('.nav-item');
 
 // Function for responsive burger menu
 burgerMenu.addEventListener('click', () => {
-  if (navItemClass.style.display === 'none') {
-    document.querySelectorAll('.nav-item').forEach(el => el.style.display = 'block');
-  } else {
+  if (navItemClass.style.display === 'block') {
     document.querySelectorAll('.nav-item').forEach(el => el.style.display = 'none');
+  } else {
+    document.querySelectorAll('.nav-item').forEach(el => el.style.display = 'block');
   }
 })
 
