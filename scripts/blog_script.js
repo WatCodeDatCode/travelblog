ed65b289cb99f3cded79d10b14140dc97bdb3ee9
Andrew Russell <62206738+WatCodeDatCode@users.noreply.github.com>
2021-01-29 14:50:26 +0100

    Fixes API issue

--- scripts/blog_script.js
+++ scripts/blog_script.js
@@ -49,16 +49,10 @@ burgerMenu.addEventListener("click", () => {
   }
 });
 
-// Variable for weather API
-const apiKey = process.env.WEATHER_API;
-
-// Variable for Google API
-const mapApiKey = process.env.MAP_API;
-
 // Async function to fetch weather from API
 async function getWeatherInformation(city) {
   let response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=03bbfddd33521d0c17e64ea09b10e111`
   );
   let data = await response.json();
   return data;
@@ -313,7 +307,7 @@ async function createSingleEntry(entry) {
     <iframe
       class="h-full min-h-30 w-full col-span-3 md:col-span-2"
       frameborder="0" style="border:0"
-      src="https://www.google.com/maps/embed/v1/place?key=${mapApiKey}&q=${
+      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCLtPYNl2W9WNdJVCMjXURmGwnTJrTaeOQ&q=${
     entry.city
   }" allowfullscreen>
     </iframe>
